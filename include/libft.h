6fb3c1d784fad22e57cf2dfb25a7f95887622cd0
Matthew_Dreemurr <mhadad@gmail.com>
2021-10-05 18:35:57 +0200

    wip

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/01 15:47:47 by mahadad           #+#    #+#             */
-/*   Updated: 2021/10/05 18:26:08 by mahadad          ###   ########.fr       */
+/*   Updated: 2021/10/05 18:29:26 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,8 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlcat(char *restrict d, const char *restrict s, size_t dstsize);
-char	*strchr(const char *s, int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 
 /*=={ ctype.h }==*/
 
