64cfae66738c2401fb3afd628116e9f0c8812d55
mahadad@19 <matth.hadad@gmail.com>
2021-10-13 09:45:07 +0200

    wip

--- srcs/ft_lstdelone.c
+++ srcs/ft_lstdelone.c
@@ -6,14 +6,16 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/12 17:40:01 by mahadad           #+#    #+#             */
-/*   Updated: 2021/10/12 17:42:02 by mahadad          ###   ########.fr       */
+/*   Updated: 2021/10/13 09:43:00 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+#include <stdlib.h>
+
 /**
  * @brief Takes as a parameter an element and frees thememory of the element’s
- *        content using the function ’del’given as a parameter and free the
+ *        content using the function ’del’ given as a parameter and free the
  *        element.
  *        The memory of ’next’ must not be freed.
  * 
@@ -22,5 +24,30 @@
  */
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
+	if (!del || !lst)
+		return ;
+	del(lst->content);
+	free(lst->content);
+}
 
+#include <string.h>
+#include <stdio.h>
+
+int	main()
+{
+	char **test;
+
+	test = (char **)malloc(sizeof(char *) * 5);
+	bzero(test, sizeof(char *) * 5);
+	for (int i = 0; i < 5; i++)
+	{
+		test[i] = (char *)malloc(32);
+		for (size_t y = 0; y < 32; y++)
+		{
+			test[i][y] = y+'a';
+		}
+		test[i][31] = 0;
+		printf("test[%d]|%s|\n", i, test[i]);
+	}
+	return 0;
 }
