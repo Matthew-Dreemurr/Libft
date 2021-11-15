3dc66bc0afff9967a339fb18fefbca854616fad8
Matthew-Dreemurr <mahadad@student.s19.be>
2021-11-15 11:37:38 +0100

    Makefile push 2021-11-15 11:37:38

--- src/vector/vect_resize.c
+++ src/vector/vect_resize.c
@@ -6,13 +6,20 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 13:24:27 by mahadad           #+#    #+#             */
-/*   Updated: 2021/11/12 16:51:57 by mahadad          ###   ########.fr       */
+/*   Updated: 2021/11/15 11:36:02 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+/**
+ * @brief Resize the buffer wit `size`.
+ * 
+ * @param v 
+ * @param size 
+ * @return char* 
+ */
 char	*vect_resize(t_vector *v, size_t size)
 {
 	char	*tmp;
@@ -23,7 +30,10 @@ char	*vect_resize(t_vector *v, size_t size)
 	v->max = size;
 	v->buff = (char *)malloc(v->max);
 	if (!v->buff)
+	{
+		v->buff = tmp;
 		return (VEC_EXIT_FAILURE);
+	}
 	ft_memcpy(v->buff, tmp, v->len);
 	free(tmp);
 	return (v->buff);
