cdd8771e030277585c8acd7ed86414fe5e9889cb
Matthew-Dreemurr <hadadmat@gmail.com>
2021-12-05 00:25:35 +0100

    Makefile push 2021-12-05 00:25:35

--- src/string/charsetchar.c
+++ src/string/charsetchar.c
@@ -6,7 +6,7 @@
 /*   By: mahadad <mahadad@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/03 21:10:16 by mahadad           #+#    #+#             */
-/*   Updated: 2021/12/03 22:41:49 by mahadad          ###   ########.fr       */
+/*   Updated: 2021/12/05 00:24:59 by mahadad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 /**
  * @brief Will check if the char is composed with one of the character in `set`.
+ *         Only
  * 
  * @param c The char.
  * @param set The caracter set.
- * @return int `1` if all the char in `str` is composed with the char in `set`,
- *              else `0`.
+ * @return int the index if the char in `set`, else `-1`.
  */
 int	charsetchar(char c, char *set)
 {
@@ -32,6 +32,6 @@ int	charsetchar(char c, char *set)
 		i++;
 	}
 	if (!set[i])
-		return (0);
-	return (1);
+		return (-1);
+	return (i);
 }
